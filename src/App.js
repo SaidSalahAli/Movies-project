650593c7ba99184173a9ac73d3037c540c60e15a
SaidSalahAli <saidsalah375@gmail.com>
2023-01-27 23:06:39 +0200

    deploy project

--- src/App.js
+++ src/App.js
@@ -1,23 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+import MovieList from "./components/MovieList";
+import NavBar from "./components/NavBar";
+import { Container } from "react-bootstrap";
+
+// import { useState } from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import MovieDetails from "./components/MovieDetails";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <NavBar />
+      <Container>
+        <BrowserRouter basename= {window.location.pathname ||""}>
+          <Routes>
+            <Route path="/" element={<MovieList />} />
+            <Route path="/movie/:id"  element={<MovieDetails />} />
+          </Routes>
+        </BrowserRouter>
+      </Container>
     </div>
   );
 }
